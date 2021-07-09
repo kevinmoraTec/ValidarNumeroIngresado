d573c3dd37a1133761e6d9aee29ef5a0f24083b4
@DanielMora10 <djkevindaniel2@gmail.com>
2021-07-09 15:13:15 -0500

    Primera Ventana Ingrsar numero

---
+++ app/src/main/java/com/example/numeroalazar/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.numeroalazar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.inicio)
+        R.color.black
+
+    }
+}
