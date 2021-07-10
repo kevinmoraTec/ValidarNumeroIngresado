c05430ba98a657721ad742f27bf68fe826d42c00
@DanielMora10 <djkevindaniel2@gmail.com>
2021-07-10 14:40:50 -0500

    Validar

--- app/src/main/java/com/example/numeroalazar/MainActivity.java
+++ app/src/main/java/com/example/numeroalazar/MainActivity.java
@@ -4,14 +4,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     private Button validarRespuesta;
-
+    private TextView numeroU;
+    public int numeroUsuario;
+    private int numR;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        // Union xml
+        numeroU =(TextView) findViewById(R.id.tvinicio);
+        numR=crearAleatorio();
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.inicio);
@@ -21,8 +28,40 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent i = new Intent(MainActivity.this, palatially.class);
                 startActivity(i);
+                numeroUsuario= Integer.parseInt(numeroU.toString());
+
+
             }
         });
     }
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
 
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    public int crearAleatorio(){
+     return (int) (Math.random()*10+1);
+    }
+    public void validarRept(){
+        if(numR == numeroUsuario){
+            System.out.println("no es valido");
+            System.out.println(numR);
+            System.out.println(numeroUsuario);
+        }
+    }
 }
--- app/src/main/java/com/example/numeroalazar/palatially.java
+++ app/src/main/java/com/example/numeroalazar/palatially.java
@@ -1,22 +1,59 @@
 package com.example.numeroalazar;
+import com.example.numeroalazar.MainActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class palatially extends AppCompatActivity {
+    private Button regresar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        
 
+        
         super.onCreate(savedInstanceState);
         setContentView(R.layout.validar);
+        regresar=findViewById(R.id.botoregresar);
+        regresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(palatially.this,MainActivity.class);
+                startActivity(i);
+            }
+        });
 
 
-
-        }
     }
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
+
+
+
 
 
